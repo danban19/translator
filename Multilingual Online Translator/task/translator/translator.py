6ec39f12a9d41a309a4a29b94196c0db60268201
danban19 <daniel.banasik1@gmail.com>
2021-02-01 23:06:15 +0100

    add new functionality and possibility of using the program from the command line

--- Multilingual Online Translator/task/translator/translator.py
+++ Multilingual Online Translator/task/translator/translator.py
@@ -1,5 +1,7 @@
 import requests
+import sys
 from bs4 import BeautifulSoup
+
 
 class Translator:
 
@@ -18,22 +20,33 @@ class Translator:
 11. Romanian
 12. Russian
 13. Turkish''')
+        self.arguments = sys.argv
+        self.languages = {'1': 'Arabic', '2': 'German', '3': 'English', '4': 'Spanish', '5': 'French',
+                          '6': 'Hebrew', '7': 'Japanese', '8': 'Dutch', '9': 'Polish', '10': 'Portuguese',
+                          '11': 'Romanian', '12': 'Russian', '13': 'Turkish'}
+        self.first_language = str(self.arguments[1]).capitalize()
+        self.second_language = str(self.arguments[2]).capitalize()
+        self.word = str(self.arguments[3])
+        print(self.first_language)
+        print(self.second_language)
+        print(self.word)
 
     def greetings(self):
-        self.first_language = input("Type the number of your language:\n")
-        self.second_language = input("Type the number of a language you want to translate to or '0' to translate to all languages:\n")
-        self.word = input("Type the word you want to translate:\n")
-        self.languages = {'1':'Arabic', '2':'German', '3': 'English', '4':'Spanish', '5':'French',
-                          '6':'Hebrew', '7':'Japanese','8': 'Dutch', '9': 'Polish', '10': 'Portuguese',
-        '11':'Romanian', '12': 'Russian', '13': 'Turkish'}
-        if self.second_language == '0':
+        if self.second_language == '0' or self.second_language == 'All':
             self.multitranslation()
         else:
             self.single_translation()
+        # self.first_language = input("Type the number of your language:\n")
+        # self.second_language = input("Type the number of a language you want to translate to or '0' "
+        #                              "to translate to all languages:\n")
+        # self.word = input("Type the word you want to translate:\n")
+
 
     def single_translation(self):
-        page = f'https://context.reverso.net/translation/{self.languages.get(self.first_language).lower()}-' \
-               f'{self.languages.get(self.second_language).lower()}/{self.word}'
+        # page = f'https://context.reverso.net/translation/{self.languages.get(self.first_language).lower()}-' \
+        #        f'{self.languages.get(self.second_language).lower()}/{self.word}'
+        page = f'https://context.reverso.net/translation/{self.first_language.lower()}-' \
+               f'{self.second_language.lower()}/{self.word}'
         user_agent = 'Mozilla/5.0'
         r = requests.get(page, headers={'User-Agent': user_agent})
         soup = BeautifulSoup(r.content, 'html.parser')
@@ -43,8 +56,8 @@ class Translator:
         examples = [p.text for p in soup.find_all('div', class_='ltr')]
         translate_file = open(f'{self.word}.txt', 'w', encoding='utf-8')
 
-        print(f'{self.languages.get(self.second_language)} Translations:')
-        translate_file.write(f'{self.languages.get(self.second_language)} Translations:')
+        print(f'{self.second_language} Translations:')
+        translate_file.write(f'{self.second_language} Translations:')
         translate_file.write('\n')
         for i in range(1, len(translations)):
             print(translations[i].strip())
@@ -53,8 +66,8 @@ class Translator:
         translate_file.write('\n\n')
         print('\n')
 
-        print(f'{self.languages.get(self.second_language)} Example:')
-        translate_file.write(f'{self.languages.get(self.second_language)} Example:')
+        print(f'{self.second_language} Example:')
+        translate_file.write(f'{self.second_language} Example:')
         translate_file.write('\n')
         for i in range(1, len(examples)):
             print(examples[i].strip())
@@ -71,7 +84,9 @@ class Translator:
         user_agent = 'Mozilla/5.0'
         translate_file = open(f'{self.word}.txt', 'w', encoding='utf-8')
         for element in self.languages.values():
-            page = f'https://context.reverso.net/translation/{self.languages.get(self.first_language).lower()}-' \
+            # page = f'https://context.reverso.net/translation/{self.languages.get(self.first_language).lower()}-' \
+            #        f'{element.lower()}/{self.word}'
+            page = f'https://context.reverso.net/translation/{self.first_language.lower()}-' \
                    f'{element.lower()}/{self.word}'
             r = requests.get(page, headers={'User-Agent': user_agent})
             soup = BeautifulSoup(r.content, 'html.parser')
@@ -101,6 +116,7 @@ class Translator:
             print('\n')
             translate_file.write('\n\n')
         translate_file.close()
+
 
 translate = Translator()
 translate.greetings()
