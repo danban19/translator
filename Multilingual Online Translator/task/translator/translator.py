539a08b5db67087f7adedf04f63df5aeddfb96a3
danban19 <daniel.banasik1@gmail.com>
2021-03-03 14:36:51 +0100

    add exceptions handling functionality, refactor the code, add comments

--- Multilingual Online Translator/task/translator/translator.py
+++ Multilingual Online Translator/task/translator/translator.py
@@ -5,8 +5,8 @@ from bs4 import BeautifulSoup
 
 class Translator:
 
-    def __init__(self):
-        print('''Hello, you're welcome to the translator. Translator supports: 
+    def __init__(self, arguments):
+        self.greets = '''Hello, you're welcome to the translator. Translator supports: 
 1. Arabic
 2. German
 3. English
@@ -19,104 +19,86 @@ class Translator:
 10. Portuguese
 11. Romanian
 12. Russian
-13. Turkish''')
-        self.arguments = sys.argv
-        self.languages = {'1': 'Arabic', '2': 'German', '3': 'English', '4': 'Spanish', '5': 'French',
-                          '6': 'Hebrew', '7': 'Japanese', '8': 'Dutch', '9': 'Polish', '10': 'Portuguese',
-                          '11': 'Romanian', '12': 'Russian', '13': 'Turkish'}
+13. Turkish\n'''
+        self.languages = ['Arabic', 'German', 'English', 'Spanish', 'French', 'Hebrew', 'Japanese', 'Dutch',
+                          'Polish', 'Portuguese', 'Romanian', 'Russian', 'Turkish', 'All']
+        self.arguments = arguments
         self.first_language = str(self.arguments[1]).capitalize()
         self.second_language = str(self.arguments[2]).capitalize()
         self.word = str(self.arguments[3])
-        print(self.first_language)
-        print(self.second_language)
-        print(self.word)
+        self.translate_file = open(f'{self.word}.txt', 'w', encoding='utf-8')
+        self.input_check()
 
-    def greetings(self):
-        if self.second_language == '0' or self.second_language == 'All':
-            self.multitranslation()
+    def input_check(self):  # checks if input languages and word are valid
+        try:
+            self.languages.index(self.first_language)
+        except ValueError:
+            print(f"Sorry, the program doesn't support {self.first_language.lower()}")
+            sys.exit()
+        try:
+            self.languages.index(self.second_language)
+        except ValueError:
+            print(f"Sorry, the program doesn't support {self.second_language.lower()}")
+            sys.exit()
         else:
-            self.single_translation()
-        # self.first_language = input("Type the number of your language:\n")
-        # self.second_language = input("Type the number of a language you want to translate to or '0' "
-        #                              "to translate to all languages:\n")
-        # self.word = input("Type the word you want to translate:\n")
+            self.translation_choice()
 
+    def translation_choice(self):
+        if self.second_language == '0' or self.second_language == 'All':
+            self.multi_translation(self.first_language, self.word)
+        else:
+            print(self.greets)
+            self.translate_file.write(f'{self.greets}\n')
+            self.single_translation(self.first_language, self.second_language, self.word)
 
-    def single_translation(self):
-        # page = f'https://context.reverso.net/translation/{self.languages.get(self.first_language).lower()}-' \
-        #        f'{self.languages.get(self.second_language).lower()}/{self.word}'
-        page = f'https://context.reverso.net/translation/{self.first_language.lower()}-' \
-               f'{self.second_language.lower()}/{self.word}'
+    def url_scrapper(self, first_language, second_language, word):  # scraps the website, checks connection
+        page = f'https://context.reverso.net/translation/{first_language.lower()}-' \
+               f'{second_language.lower()}/{word}'
         user_agent = 'Mozilla/5.0'
         r = requests.get(page, headers={'User-Agent': user_agent})
         soup = BeautifulSoup(r.content, 'html.parser')
-        if r.status_code == 200:
-            print(r.status_code, 'OK\n')
-        translations = [p.text for p in soup.find_all('a', class_='translation')]
-        examples = [p.text for p in soup.find_all('div', class_='ltr')]
-        translate_file = open(f'{self.word}.txt', 'w', encoding='utf-8')
+        translations = [p.text for p in soup.find_all('a', class_='translation')][1:]
+        examples = [p.text for p in soup.find_all('div', class_='src ltr')]
+        try:
+            if r.status_code == 404:
+                raise ValueError
+            elif r.status_code != 200:
+                raise ConnectionError
+        except ValueError:
+            print(f'Sorry, unable to find {self.word}')
+            sys.exit()
+        except ConnectionError:
+            print('Something wrong with your internet connection')
+            sys.exit()
+        else:
+            return translations, examples, r.status_code
 
-        print(f'{self.second_language} Translations:')
-        translate_file.write(f'{self.second_language} Translations:')
-        translate_file.write('\n')
-        for i in range(1, len(translations)):
-            print(translations[i].strip())
-            translate_file.write(translations[i].strip())
-            translate_file.write('\n')
-        translate_file.write('\n\n')
-        print('\n')
+    def single_translation(self, first_language, second_language, word):
+        translation_soup = self.url_scrapper(first_language, second_language, word)
+        translations = translation_soup[0]
+        examples = translation_soup[1]
+        print(f'\n{second_language} Translations:')
+        print(f'\n{second_language} Translations:', file=self.translate_file)
+        length_tr = 3 if len(translations) > 3 else len(translations)
+        length_ex = 3 if len(examples) > 3 else len(examples)
+        for i in range(length_tr):
+            print(translations[i].strip(), end='\n')
+            print(translations[i].strip(), end='\n', file=self.translate_file)
+        print(f'\n{second_language} Example:')
+        print(f'\n{second_language} Example:', file=self.translate_file)
+        for i in range(1, length_ex):
+            print(examples[i].strip(), end='\n')
+            print(examples[i].strip(), end='\n', file=self.translate_file)
 
-        print(f'{self.second_language} Example:')
-        translate_file.write(f'{self.second_language} Example:')
-        translate_file.write('\n')
-        for i in range(1, len(examples)):
-            print(examples[i].strip())
-            translate_file.write(examples[i].strip())
-            translate_file.write('\n')
-            if i % 2 == 0:
-                print('\n')
-                translate_file.write('\n\n')
-        print('\n')
-        translate_file.write('\n\n')
-        translate_file.close()
-
-    def multitranslation(self):
-        user_agent = 'Mozilla/5.0'
-        translate_file = open(f'{self.word}.txt', 'w', encoding='utf-8')
-        for element in self.languages.values():
-            # page = f'https://context.reverso.net/translation/{self.languages.get(self.first_language).lower()}-' \
-            #        f'{element.lower()}/{self.word}'
-            page = f'https://context.reverso.net/translation/{self.first_language.lower()}-' \
-                   f'{element.lower()}/{self.word}'
-            r = requests.get(page, headers={'User-Agent': user_agent})
-            soup = BeautifulSoup(r.content, 'html.parser')
-            translations = [p.text for p in soup.find_all('a', class_='translation')]
-            examples = [p.text for p in soup.find_all('div', class_='ltr')]
-
-            print(f'{element} Translations:')
-            translate_file.write(f'{element} Translations:')
-            translate_file.write('\n')
-            for i in range(1, len(translations)):
-                print(translations[i].strip())
-                translate_file.write(translations[i].strip())
-                translate_file.write('\n')
-            translate_file.write('\n\n')
-            print('\n')
-
-            print(f'{element} Example:')
-            translate_file.write(f'{element} Example:')
-            translate_file.write('\n')
-            for i in range(1, len(examples)):
-                print(examples[i].strip())
-                translate_file.write(examples[i].strip())
-                translate_file.write('\n')
-                if i % 2 == 0:
-                    print('\n')
-                    translate_file.write('\n\n')
-            print('\n')
-            translate_file.write('\n\n')
-        translate_file.close()
+    def multi_translation(self, first_language, word):
+        # iterates through all the languages using for loop and single_translation method
+        print(self.greets)
+        print(self.greets, file=self.translate_file)
+        for element in self.languages[:-1]:
+            self.single_translation(first_language, element, word)
+        self.translate_file.close()
+        sys.exit()
 
 
-translate = Translator()
-translate.greetings()
+translation = Translator(sys.argv)
+translation.input_check()
